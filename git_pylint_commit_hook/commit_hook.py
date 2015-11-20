ca53fee724662f3afe26e7ceca513b6a8d41ff36
Michael Kefeder <mike@multiwave.ch>
2015-11-20 18:29:28 +0100

    compatible to python 2.7 and python 3.4 now, code refactored to output no pylint messages (eating my own dog food)

--- git_pylint_commit_hook/commit_hook.py
+++ git_pylint_commit_hook/commit_hook.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 """ Commit hook for pylint """
 import decimal
 import os
@@ -5,8 +6,10 @@ import re
 import sys
 import subprocess
 import collections
-import ConfigParser
-
+try:
+    import ConfigParser
+except ImportError:
+    import configparser as ConfigParser
 
 ExecutionResult = collections.namedtuple(
     'ExecutionResult',
@@ -15,6 +18,7 @@ ExecutionResult = collections.namedtuple(
 
 
 def _execute(cmd):
+    """Executes command and returns status, stdout and stderr"""
     process = subprocess.Popen(
         cmd,
         stdout=subprocess.PIPE,
@@ -26,6 +30,7 @@ def _execute(cmd):
 
 
 def _current_commit():
+    """asks git for current commit"""
     if _execute('git rev-parse --verify HEAD'.split()).status:
         return '4b825dc642cb6eb9a060e54bf8d69288fbee4904'
     else:
@@ -36,13 +41,13 @@ def _get_list_of_committed_files():
     """ Returns a list of files about to be commited. """
     files = []
     # pylint: disable=E1103
-    diff_index_cmd = 'git diff-index --cached %s' % _current_commit()
+    diff_index_cmd = 'git diff-index %s' % _current_commit()
     output = subprocess.check_output(
         diff_index_cmd.split()
     )
-    for result in output.split('\n'):
-        if result != '':
-            result = result.split()
+    for result in output.decode("utf-8", "ignore").split('\n'):
+        if result:
+            result = str(result).split()
             if result[4] in ['A', 'M']:
                 files.append(result[5])
 
@@ -74,12 +79,38 @@ def _parse_score(pylint_output):
     If the score is not found, return 0.0.
 
     """
-    for line in pylint_output.splitlines():
+    for line in pylint_output.decode('utf-8').splitlines():
         match = re.match(_SCORE_REGEXP, line)
         if match:
             return float(match.group(1))
     return 0.0
 
+
+def _read_config(pylintrc, pylint, pylint_params, limit):
+    """reads from pylintrc"""
+    if os.path.exists(pylintrc):
+        conf = ConfigParser.SafeConfigParser()
+        conf.read(pylintrc)
+        if conf.has_option('pre-commit-hook', 'command'):
+            pylint = conf.get('pre-commit-hook', 'command')
+        if conf.has_option('pre-commit-hook', 'params'):
+            pylint_params = '' if not pylint_params else pylint_params
+            pylint_params += ' ' + conf.get('pre-commit-hook', 'params')
+        if conf.has_option('pre-commit-hook', 'limit'):
+            limit = float(conf.get('pre-commit-hook', 'limit'))
+    return pylint, pylint_params, limit
+
+def _collect_py_files():
+    """looks for python scripts within the list of commited files"""
+    python_files = dict()
+    for filename in _get_list_of_committed_files():
+        try:
+            if _is_python_file(filename):
+                python_files[filename] = False
+        except IOError:
+            print('File not found (probably deleted): {}\t\tSKIPPED'.format(
+                filename))
+    return python_files
 
 def check_repo(
         limit, pylint='pylint', pylintrc='.pylintrc', pylint_params=None,
@@ -97,49 +128,31 @@ def check_repo(
     :type suppress_report: bool
     :param suppress_report: Suppress report if score is below limit
     """
-    # List of checked files and their results
-    python_files = []
-
-    # Set the exit code
-    all_filed_passed = True
-
-    # Find Python files
-    for filename in _get_list_of_committed_files():
-        try:
-            if _is_python_file(filename):
-                python_files.append((filename, None))
-        except IOError:
-            print 'File not found (probably deleted): {}\t\tSKIPPED'.format(
-                filename)
+    # dictionary of filenames giving info of success
+    python_files = _collect_py_files()
 
     # Don't do anything if there are no Python files
     if len(python_files) == 0:
+        print("no python files found")
         sys.exit(0)
 
     # Load any pre-commit-hooks options from a .pylintrc file (if there is one)
-    if os.path.exists(pylintrc):
-        conf = ConfigParser.SafeConfigParser()
-        conf.read(pylintrc)
-        if conf.has_option('pre-commit-hook', 'command'):
-            pylint = conf.get('pre-commit-hook', 'command')
-        if conf.has_option('pre-commit-hook', 'params'):
-            pylint_params += ' ' + conf.get('pre-commit-hook', 'params')
-        if conf.has_option('pre-commit-hook', 'limit'):
-            limit = float(conf.get('pre-commit-hook', 'limit'))
+    pylint, pylint_params, limit = _read_config(pylintrc, pylint, pylint_params, limit)
 
-    # Pylint Python files
     i = 1
-    for python_file, score in python_files:
+    outfile_fn = '/tmp/pylint_hook_output'
+    outfile = open(outfile_fn, 'wb')
+    for python_file in python_files.keys():
         # Allow __init__.py files to be completely empty
-        if os.path.basename(python_file) == '__init__.py':
-            if os.stat(python_file).st_size == 0:
-                print(
-                    'Skipping pylint on {} (empty __init__.py)..'
-                    '\tSKIPPED'.format(python_file))
-
-                # Bump parsed files
-                i += 1
-                continue
+        if os.path.basename(python_file) == '__init__.py' \
+            and os.path.getsize(python_file) == 0:
+            print(
+                'Skipping pylint on {} (empty __init__.py)..'
+                '\tSKIPPED'.format(python_file))
+            # Bump parsed files
+            python_files[python_file] = True
+            i += 1
+            continue
 
         # Start pylinting
         sys.stdout.write("Running pylint on {} (file {}/{})..\t".format(
@@ -155,40 +168,32 @@ def check_repo(
             else:
                 command.append('--rcfile={}'.format(pylintrc))
 
-
             command.append(python_file)
 
-            proc = subprocess.Popen(
-                command,
-                stdout=subprocess.PIPE,
-                stderr=subprocess.PIPE)
-            out, _ = proc.communicate()
+            result = _execute(command)
         except OSError:
             print("\nAn error occurred. Is pylint installed?")
             sys.exit(1)
 
         # Verify the score
-        score = _parse_score(out)
+        score = _parse_score(result.stdout)
         if score >= float(limit):
             status = 'PASSED'
+            python_files[python_file] = True
         else:
             status = 'FAILED'
-            all_filed_passed = False
 
         # Add some output
         print('{:.2}/10.00\t{}'.format(decimal.Decimal(score), status))
         if 'FAILED' in status:
             if suppress_report:
                 command.append('--reports=n')
-                proc = subprocess.Popen(
-                    command,
-                    stdout=subprocess.PIPE,
-                    stderr=subprocess.PIPE)
-                out, _ = proc.communicate()
-            print out
-
+                result = _execute(command)
+            outfile.write(result.stdout)
 
         # Bump parsed files
         i += 1
-
-    return all_filed_passed
+    outfile.close()
+    if False in python_files.values():
+        print('Look at %s for details' % outfile_fn)
+    return False not in python_files.values()
